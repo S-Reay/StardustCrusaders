89cb5be0d8cf09a693c056fb30e13dc44fad5438
Seth Reay <1017464@student.sae.edu.au>
2020-02-04 11:48:31 +1100

    Shooting, Enemies
    Added viewmodel. Added shooting mechanics, enemy health, enemy death. Added muzzleflash particle system.

---
+++ CyberVikingPrototype/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 50f;
+    public Rigidbody rb;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+    }
+}
